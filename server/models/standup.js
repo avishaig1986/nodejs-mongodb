eb2fbee5980ff08c97dfde32689ad0dd74413f8e
unknown <avishaig1986@outlook.com>
2020-02-05 17:12:13 +0200

    adding validation for mandatory fields

--- server/models/standup.js
+++ server/models/standup.js
@@ -1,15 +1,35 @@
 const mongoose = require('mongoose')
 
+const requiredStringValidator = [
+  function (val){
+    let testVal = val.trim()
+    return (testVal.length > 0)
+  },
+  //Custom error text
+  'Please supply a value for {PATH}'
+]
+
 const standupSchema = new mongoose.Schema({
   teamMemberId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'teamMembers'
   },
-  teamMember: { type: String },
-  project: { type: String },
-  workYesterday: { type: String },
-  workToday: { type: String },
-  impediment: { type: String }, 
+  teamMember: { 
+    type: String,
+    required: true,
+    validate: requiredStringValidator },
+  project: { type: String,
+    required: true,
+    validate: requiredStringValidator },
+  workYesterday: { type: String,
+    required: true,
+    validate: requiredStringValidator },
+  workToday: { type: String,
+    required: true,
+    validate: requiredStringValidator },
+  impediment: { type: String,
+    required: true,
+    validate: requiredStringValidator },
   createdOn: { type: Date, default: Date.now }
   })
 
